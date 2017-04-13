c0f9ffc922d5a7d057df241612ea3f87142f3bef
Tommy Ho <yytommyho@link.cuhk.edu.hk>
2017-04-13 19:25:33 +0800

    merged by tommy (recording and main)

--- app/src/main/java/com/example/videomaps/RecordActivity.java
+++ app/src/main/java/com/example/videomaps/RecordActivity.java
@@ -1,141 +1,305 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.videomaps;
 
-import android.content.Intent;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.graphics.Color;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.videomaps.CameraHelper;
+import com.example.videomaps.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.TimeZone;
-
-import android.media.MediaRecorder;
-import android.os.Bundle;
-import android.os.Environment;
-import android.support.v4.app.FragmentActivity;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import java.io.IOException;
+import java.util.List;
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
+ *  This activity uses the camera/camcorder as the A/V source for the {@link android.media.MediaRecorder} API.
+ *  A {@link android.view.TextureView} is used as the camera preview which limits the code to API 14+. This
+ *  can be easily replaced with a {@link android.view.SurfaceView} to run on older devices.
  */
-public class RecordActivity extends FragmentActivity implements SurfaceHolder.Callback {
-    private Camera svCam;
-    private SurfaceView svCamPreview;
-    private SurfaceHolder svCamHolder;
-    private MediaRecorder svRecorder;
-    private boolean isRecording=false;
-    private int mRotateDegree;
-    private Calendar calendar = Calendar.getInstance();
-    private SimpleDateFormat dateFormat=new SimpleDateFormat("yyyyMMdd-HHmmss");
+public class RecordActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private Camera mCamera;
+    private TextureView mPreview;
+    private MediaRecorder mMediaRecorder;
+    private File mOutputFile;
+
+    private boolean isRecording = false;
+    private static final String TAG = "Recorder";
+    private ImageButton captureButton;
+
+    private GoogleMap mMap;
+
+    private TextView timer;
+    private long startHTime = 0L;
+    private Handler timerHandler = new Handler();
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
+
+
+    private Runnable updateTimerThread = new Runnable() {
+
+        public void run() {
+
+            timeInMilliseconds = SystemClock.uptimeMillis() - startHTime;
+
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            if (timer != null)
+                timer.setText("" + String.format("%02d", mins) + ":" + String.format("%02d", secs));
+            timerHandler.postDelayed(this, 0);
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
-        svCamPreview=(SurfaceView)findViewById(R.id.svCam);
-        ImageButton btnLocation=(ImageButton)findViewById(R.id.btnLocation);
-        ImageButton btnSwitch=(ImageButton)findViewById(R.id.btnSwitch);
-        ImageButton btnRecord=(ImageButton)findViewById(R.id.btnRecord);
-        svCamHolder=svCamPreview.getHolder();
-        svCamHolder.addCallback(this);
 
+        mPreview = (TextureView) findViewById(R.id.surface_view);
+        captureButton = (ImageButton) findViewById(R.id.button_capture);
+        timer = (TextView) findViewById(R.id.time);// Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.location);
+        mapFragment.getMapAsync(this);
     }
+
     @Override
-    protected void onResume(){
-        svRecorder=new MediaRecorder();
-        svCam=Camera.open();
-        super.onResume();
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-40, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    /**
+     * The capture button controls all user interaction. When recording, the button click
+     * stops recording, releases {@link android.media.MediaRecorder} and {@link android.hardware.Camera}. When not recording,
+     * it prepares the {@link android.media.MediaRecorder} and starts recording.
+     *
+     * @param view the view generating the event.
+     */
+    public void onCaptureClick(View view) {
+        if (isRecording) {
+            // BEGIN_INCLUDE(stop_release_media_recorder)
+
+            // stop recording and release camera
+            try {
+                mMediaRecorder.stop();  // stop the recording
+                timeSwapBuff += timeInMilliseconds;
+                timerHandler.removeCallbacks(updateTimerThread);
+            } catch (RuntimeException e) {
+                // RuntimeException is thrown when stop() is called immediately after start().
+                // In this case the output file is not properly constructed ans should be deleted.
+                Log.d(TAG, "RuntimeException: stop() is called immediately after start()");
+                //noinspection ResultOfMethodCallIgnored
+                mOutputFile.delete();
+            }
+            releaseMediaRecorder(); // release the MediaRecorder object
+            mCamera.lock();         // take camera access back from MediaRecorder
+
+            // inform the user that recording has stopped
+            isRecording = false;
+            setCaptureButtonStatus();
+            releaseCamera();
+            // END_INCLUDE(stop_release_media_recorder)
+
+        } else {
+
+            // BEGIN_INCLUDE(prepare_start_media_recorder)
+
+            new MediaPrepareTask().execute(null, null, null);
+
+            // END_INCLUDE(prepare_start_media_recorder)
+
+        }
+    }
+
+    private void setCaptureButtonStatus() {
+        if (isRecording)
+            captureButton.setImageDrawable(getResources().getDrawable(R.drawable.camera_stop));
+        else
+            captureButton.setImageDrawable(getResources().getDrawable(R.mipmap.camera_record));
+    }
+
     @Override
-    protected  void onPause(){
-        svCam.stopPreview();
-        svCam.release();
-        svCam=null;
+    protected void onPause() {
         super.onPause();
+        // if we are using MediaRecorder, release it first
+        releaseMediaRecorder();
+        // release the camera immediately on pause event
+        releaseCamera();
     }
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder){
-        try{
-            svCam.setPreviewDisplay(svCamHolder);
 
-            Camera.CameraInfo camInfo = new Camera.CameraInfo();
-            Camera.getCameraInfo(0, camInfo);
-
-            int rotation = getWindowManager().getDefaultDisplay().getRotation();
-            int degrees = 0;
-            switch (rotation) {
-                case Surface.ROTATION_0:
-                    degrees = 0; break;
-                case Surface.ROTATION_90:
-                    degrees = 90; break;
-                case Surface.ROTATION_180:
-                    degrees = 180; break;
-                case Surface.ROTATION_270:
-                    degrees = 270; break;
-            }
-
-            mRotateDegree = (camInfo.orientation - degrees + 360) % 360;
-            svCam.setDisplayOrientation(mRotateDegree);
-
-            svCam.startPreview();
-
-            Camera.Parameters camParas = svCam.getParameters();
-            if (camParas.getFocusMode().equals(Camera.Parameters.FOCUS_MODE_AUTO) ||
-                    camParas.getFocusMode().equals(Camera.Parameters.FOCUS_MODE_MACRO))
-                svCam.autoFocus(null);
-            else
-                Toast.makeText(this, "Auto focus is unsupport", Toast.LENGTH_SHORT).show();
-        }catch(Exception e){
-            Intent actMain=new Intent();
-            actMain.setClass(RecordActivity.this,MainActivity.class);
-            Toast.makeText(this,"Camera Error",Toast.LENGTH_SHORT).show();
-            startActivity(actMain);
-            finish();
-            return;
+    private void releaseMediaRecorder(){
+        if (mMediaRecorder != null) {
+            // clear recorder configuration
+            mMediaRecorder.reset();
+            // release the recorder object
+            mMediaRecorder.release();
+            mMediaRecorder = null;
+            // Lock camera for later use i.e taking it back from MediaRecorder.
+            // MediaRecorder doesn't need it anymore and we will release it if the activity pauses.
+            mCamera.lock();
         }
     }
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder,int format,int height, int width){
 
-    }
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder){
-        if(isRecording){
-            svRecorder.stop();
-            isRecording=false;
+    private void releaseCamera(){
+        if (mCamera != null){
+            // release the camera for other applications
+            mCamera.release();
+            mCamera = null;
         }
-        svRecorder.release();
-        finish();
     }
-    
-    private void startRecording() {
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private boolean prepareVideoRecorder(){
+
+        // BEGIN_INCLUDE (configure_preview)
+        mCamera = CameraHelper.getDefaultCameraInstance();
+
+        // We need to make sure that our preview and recording video size are supported by the
+        // camera. Query camera to find all the sizes and choose the optimal size given the
+        // dimensions of our preview surface.
+        Camera.Parameters parameters = mCamera.getParameters();
+        List<Camera.Size> mSupportedPreviewSizes = parameters.getSupportedPreviewSizes();
+        List<Camera.Size> mSupportedVideoSizes = parameters.getSupportedVideoSizes();
+        Camera.Size optimalSize = CameraHelper.getOptimalVideoSize(mSupportedVideoSizes,
+                mSupportedPreviewSizes, mPreview.getWidth(), mPreview.getHeight());
+
+        // Use the same size for recording profile.
+        CamcorderProfile profile = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
+        profile.videoFrameWidth = optimalSize.width;
+        profile.videoFrameHeight = optimalSize.height;
+
+        // likewise for the camera object itself.
+        parameters.setPreviewSize(profile.videoFrameWidth, profile.videoFrameHeight);
+        mCamera.setParameters(parameters);
         try {
-            File folder=new File(Environment.getExternalStorageDirectory()+"/MVM");
-            boolean isSucess=true;
-            if(!folder.exists())
-                isSucess=folder.mkdir();
-            if(!isSucess) {
-                Toast.makeText(this,"Storage Opening Error",Toast.LENGTH_SHORT).show();
-                return;
+            // Requires API level 11+, For backward compatibility use {@link setPreviewDisplay}
+            // with {@link SurfaceView}
+            mCamera.setPreviewTexture(mPreview.getSurfaceTexture());
+        } catch (IOException e) {
+            Log.e(TAG, "Surface texture is unavailable or unsuitable" + e.getMessage());
+            return false;
+        }
+        // END_INCLUDE (configure_preview)
+
+
+        // BEGIN_INCLUDE (configure_media_recorder)
+        mMediaRecorder = new MediaRecorder();
+
+        // Step 1: Unlock and set camera to MediaRecorder
+        mCamera.unlock();
+        mMediaRecorder.setCamera(mCamera);
+
+        // Step 2: Set sources
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT );
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        // Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
+        mMediaRecorder.setProfile(profile);
+
+        // Step 4: Set output file
+        mOutputFile = CameraHelper.getOutputMediaFile(CameraHelper.MEDIA_TYPE_VIDEO);
+        if (mOutputFile == null) {
+            return false;
+        }
+        mMediaRecorder.setOutputFile(mOutputFile.getPath());
+        // END_INCLUDE (configure_media_recorder)
+
+        // Step 5: Prepare configured MediaRecorder
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            Log.d(TAG, "IllegalStateException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            Log.d(TAG, "IOException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Asynchronous task for preparing the {@link android.media.MediaRecorder} since it's a long blocking
+     * operation.
+     */
+    class MediaPrepareTask extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            // initialize video camera
+            if (prepareVideoRecorder()) {
+                // Camera is available and unlocked, MediaRecorder is prepared,
+                // now you can start recording
+                mMediaRecorder.start();
+
+                isRecording = true;
+                startHTime = SystemClock.uptimeMillis();
+                timerHandler.postDelayed(updateTimerThread, 0);
+            } else {
+                // prepare didn't work, release the camera
+                releaseMediaRecorder();
+                return false;
             }
-            dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-            String fileName="mvm_"+dateFormat.format(calendar.getTime())+".mp4";
-            svRecorder.setCamera(svCam);
-            svRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            svRecorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
-            svRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-            svRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB);
-            svRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
-            svRecorder.setOutputFile(folder+"/" +fileName);
-            svRecorder.setPreviewDisplay(svCamHolder.getSurface());
-            svRecorder.setOrientationHint(mRotateDegree);
-            svRecorder.prepare();
-            svRecorder.start();
-        } catch (Exception e) {
-            Toast.makeText(this, "Recording error", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (!result) {
+                RecordActivity.this.finish();
+            }
+            // inform the user that recording has started
+            setCaptureButtonStatus();
         }
     }
+
 }
